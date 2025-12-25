f4fb859da36e3b462e9a973f6b81481b3d976d9a
DenisTernovoy <denis.ternovoi@mail.ru>
2025-12-25 14:11:46 +0300

    Добавлены модели пользователя и привычки

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-^zm56@@w5n*gkj7l%gyhi=ul!50%xe9f-y!taxy56l0asfj($9"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG") == "True"
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "users",
+    "tracker",
 ]
 
 MIDDLEWARE = [
@@ -74,8 +80,10 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
     }
 }
 
@@ -115,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+AUTH_USER_MODEL = "users.User"
---
+++ tracker/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+from users.models import User
+
+
+class Habit(models.Model):
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name="Пользователь"
+    )
+    place = models.CharField(max_length=255, verbose_name="Место")
+    time = models.TimeField(verbose_name="Время")
+    action = models.CharField(max_length=255, verbose_name="Действие")
+    pleasant_habit = models.BooleanField(verbose_name="Приятная привычка")
+    linked_habit = models.ForeignKey(
+        "self",
+        models.SET_NULL,
+        verbose_name="Связанная привычка",
+        blank=True,
+        null=True,
+    )
+    periodicity = models.IntegerField(default=1, verbose_name="Периодичность в дн.")
+    reward = models.CharField(max_length=255, verbose_name="Вознаграждение", null=True, blank=True)
+    complete_time = models.SmallIntegerField(verbose_name="Время на выполнение в сек")
+
+    publish = models.BooleanField(verbose_name="Признак публичности", default=True)
+
+    class Meta:
+        verbose_name = "Привычка"
+        verbose_name_plural = "Привычки"
+
+    def __str__(self):
+        return f"Я буду {self.action} в {self.time} в {self.place}"
